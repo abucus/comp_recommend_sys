f654dc63d1e55b2f5aaaa8baa89391c0aa5bdec0
Mingfei Teng <tengmf@gmail.com>
2015-05-26 09:54:58 -0400

    add rmse for pimf

--- test/validate/validate_pimf.py
+++ test/validate/validate_pimf.py
@@ -11,6 +11,8 @@ import numpy as np
 import os.path as op
 from src.decomposition.pimf import PIMF
 from src.validate.measure_pimf import precision_recall
+from src.validate.measure import Measure
+from src.log import get_logger
 
 
 class Test(unittest.TestCase):
@@ -39,12 +41,20 @@ class Test(unittest.TestCase):
             total_precision.append((k,np.mean(precision)))
             total_recall.append((k,np.mean(recall))) 
         
+        
+        logger = get_logger(__name__)
+        logger.info('precision:\n{}'.format(total_precision))
+        logger.info('recall:\n{}'.format(total_recall))
+        
+        measure = Measure(R_hat = pimf.utility, R_test=np.loadtxt(op.join(base_path, 'test', 'utility')))
+        
         with open(op.join(base_path, 'result.csv'), 'wb') as f:
             writer = csv.writer(f)
             for p in total_precision:
                 writer.writerow(['precision@{}'.format(p[0]), p[1]]) 
             for r in total_recall:
-                writer.writerow(['recall@{}'.format(r[0]), r[1]])  
+                writer.writerow(['recall@{}'.format(r[0]), r[1]])
+            writer.writerow(['RMSE', measure.rmse()])
         pass
 
 
