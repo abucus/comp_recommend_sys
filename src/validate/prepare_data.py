9046e4f651cb88998b7e7ac36245b31ea2ca83cb
Mingfei Teng <tengmf@gmail.com>
2015-05-26 10:53:04 -0400

    fix bug

--- src/validate/prepare_data.py
+++ src/validate/prepare_data.py
@@ -34,10 +34,11 @@ def prepare_general_validation_data():
     generate_file(training_data, training_output)
     generate_file(test_data, test_output)
     
-def prepare_pimf_validation_data(base_path = op.join("..","..","output","data2","validate","pimf")):
+def prepare_pimf_validation_data(source_path=op.join("..", "..", "output", "data2", "original", 'simpleB.csv'), 
+                                 out_path = op.join("..","..","output","data2","validate","pimf")):
     
-    training_output = op.join(base_path, "training") 
-    test_output = op.join(base_path, "test") 
+    training_output = op.join(out_path, "training") 
+    test_output = op.join(out_path, "test") 
     if not op.exists(training_output):
         os.makedirs(training_output)
     if not op.exists(test_output):
@@ -45,7 +46,7 @@ def prepare_pimf_validation_data(base_path = op.join("..","..","output","data2",
     
     training_data_ratio = .65
     
-    training_data = read_in(source_path=op.join("..", "..", "output", "data2", "original", 'simpleB.csv'))
+    training_data = read_in(source_path)
     test_data = {'event_types':training_data['event_types'], 'table':{}}
     test_table = test_data['table']
     training_table = training_data['table']
@@ -62,4 +63,4 @@ def prepare_pimf_validation_data(base_path = op.join("..","..","output","data2",
     generate_PIMF_data(test_data, test_output)
     
 if __name__ == '__main__':
-    prepare_pimf_validation_data(base_path = op.join("..","..","output","data2","validate","pimf"))
+    prepare_pimf_validation_data(source_path=op.join("..", "..", "output", "data2", "original", 'simpleB.csv'), out_path = op.join("..","..","output","data2","validate","pimf"))
